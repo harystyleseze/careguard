20b5e09b1cae920476da913e3650cfeeec4a4971
TamerMansour-AI <teamo.palestine@gmail.com>
2026-05-13 17:22:36 +0300

    Track x402 transaction hash extraction failures

--- dashboard/src/components/tabs/activity-tab.tsx
+++ dashboard/src/components/tabs/activity-tab.tsx
@@ -262,7 +262,7 @@ export function ActivityTab({
                         </span>
                       </td>
                       <td className="px-4 py-2 text-right">
-                        <TxLink hash={tx.stellarTxHash} />
+                        <TxLink hash={tx.stellarTxHash} status={tx.txHashStatus} />
                       </td>
                     </tr>
                   ))}
